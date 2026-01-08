d83bb131e9da8ba55650909b27fb8dfa1daf1c82
Prateek Yadav <prateek246729@gmail.com>
2026-01-08 10:26:10 +0530

    Initial commit

---
+++ Backend/src/main/java/com/WebScrapping/Controller/ScrapeController.java
@@ -0,0 +1,23 @@
+package com.WebScrapping.Controller;
+
+import com.WebScrapping.Model.ScrapeResponse;
+import com.WebScrapping.Service.ScraperService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+public class ScrapeController {
+
+    @Autowired
+    private ScraperService scraperService;
+
+    @GetMapping("/api/scrape")
+    public ScrapeResponse scrapeResponse(@RequestParam String url) throws IOException {
+        return scraperService.webScrape(url);
+    }
+
+}
---
+++ Backend/src/main/java/com/WebScrapping/Service/ScraperService.java
@@ -0,0 +1,60 @@
+package com.WebScrapping.Service;
+
+import com.WebScrapping.Model.ScrapeResponse;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ScraperService {
+
+    public ScrapeResponse webScrape( String url) throws IOException {
+        Document doc = Jsoup.connect(url)
+                .userAgent("Chrome")
+                .timeout(5000)
+                .get();
+
+        List<String> images = new ArrayList<>();
+        List<String> videos = new ArrayList<>();
+        List<String> links = new ArrayList<>();
+
+//        Elements img = doc.select("a[href]");
+
+        for (Element img : doc.select("img")){
+            String src = img.absUrl("src");
+
+            if(!src.isEmpty()){
+                images.add(src);
+            }
+        }
+        for (Element video : doc.select("video")){
+            String src = video.absUrl("src");
+
+            if(!src.isEmpty()){
+                videos.add(src);
+            }
+        }
+
+        for (Element link : doc.select("a[href]")){
+            String href = link.absUrl("href");
+
+            if(!href.isEmpty()){
+                links.add(href);
+            }
+        }
+
+        ScrapeResponse response = new ScrapeResponse();
+        response.setImages(images);
+        response.setVideos(videos);
+        response.setLinks(links);
+
+        return response;
+    }
+
+}
